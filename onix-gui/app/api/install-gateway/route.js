c95d59be8ecf2359a0700e8a01f41010eccc84f6
Mishalabdullah <github.ijvob@simplelogin.co>
2024-03-30 16:13:37 +0530

    tweaking

--- onix-gui/app/api/install-gateway/route.js
+++ onix-gui/app/api/install-gateway/route.js
@@ -1,5 +1,5 @@
 import { spawn } from "child_process";
-import { NextApiResponse } from "next";
+import { NextResponse } from "next/server";
 
 export async function GET(req, res) {
   return new Promise((resolve, reject) => {
@@ -19,11 +19,11 @@ export async function GET(req, res) {
     gitProcess.on("close", (code) => {
       if (code === 0) {
         console.log("Repository cloned successfully");
-        res.status(200).json({ success: true });
+        NextResponse.status(200).json({ success: true });
         resolve();
       } else {
         console.error(`git process exited with code ${code}`);
-        res.status(500).json({ success: false });
+        NextResponse.status(500).json({ success: false });
         reject();
       }
     });
