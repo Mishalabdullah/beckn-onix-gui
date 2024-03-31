833f5ea1a09c9324610b6daf3475c5f635626c33
Mishalabdullah <github.ijvob@simplelogin.co>
2024-03-31 16:58:55 +0530

    something that atleas work. but require more work

---
+++ onix-gui/app/api/clonning-repo/route.js
@@ -0,0 +1,35 @@
+import { spawn } from "child_process";
+import { NextResponse } from "next/server";
+
+// This function is used to clone the github repository of beckn-onix
+export async function GET(req) {
+  console.log("Cloning GitHub repository...");
+  const repoUrl = "https://github.com/beckn/beckn-onix";
+  const destination = "/tmp/beckn-onix";
+  const gitProcess = spawn("git", ["clone", repoUrl, destination]);
+
+  gitProcess.stdout.on("data", (data) => {
+    console.log(`stdout: ${data}`);
+  });
+
+  gitProcess.stderr.on("data", (data) => {
+    console.error(`stderr: ${data}`);
+  });
+
+  return new Promise((resolve, reject) => {
+    gitProcess.on("close", (code) => {
+      if (code === 0) {
+        console.log("Repository cloned successfully");
+        resolve(
+          NextResponse.json(
+            { success: true, data: "Repo Cloned Successfully" },
+            { status: 200 }
+          )
+        );
+      } else {
+        console.error(`git process exited with code ${code}`);
+        resolve(NextResponse.json({ success: false }, { status: 500 }));
+      }
+    });
+  });
+}
