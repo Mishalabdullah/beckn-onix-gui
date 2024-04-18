1dee348f358f1dd5f2f03c81556d3aa89c5073a3
BlazeSN860 <virajprabhup@gmail.com>
2024-04-19 03:50:11 +0530

    feat(Slider): added-slider-component.

---
+++ onix-gui/components/Slider/Slider.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import styles from "./Slider.module.css";
+
+const Slider = ({ label, checked, toggleChecked }) => {
+    return (
+        < div className={styles.inputContainer}>
+            <label className={styles.inputLabel}>{label}</label>
+            <label class={styles.switch}>
+                <input type="checkbox" checked={checked} onChange={() => toggleChecked(!checked)} />
+                <span className={`${styles.slider} ${styles.round}`}></span>
+            </label>
+        </div >
+    );
+};
+
+export default Slider;
