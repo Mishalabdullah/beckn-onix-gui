4ea9a8e9d631bd23caec23d0b7d9a4f7b011ca3f
BlazeSN860 <virajprabhup@gmail.com>
2024-04-19 03:51:49 +0530

    feat(yaml-gen): added-yaml-gen.

---
+++ onix-gui/app/yaml-gen/page.js
@@ -0,0 +1,105 @@
+import Link from "next/link";
+import styles from "../page.module.css";
+import { Ubuntu_Mono } from "next/font/google";
+
+const ubuntuMono = Ubuntu_Mono({
+    weight: "400",
+    style: "normal",
+    subsets: ["latin"],
+});
+
+
+export default function YamlGen() {
+    return (
+        <>
+            <main className={ubuntuMono.className}>
+                <div className={styles.mainContainer}>
+                    <p className={styles.mainText}><b>Yaml File Generator</b></p>
+                    <div className={styles.boxesContainer}>
+                        <Link
+                            href="yaml-gen/option"
+                            style={{ textDecoration: "underline", color: "white" }}
+                        >
+                            <div className={styles.smallbox}>
+                                <img src="/arrow.png" />
+                                <p className={styles.boxText}>Search</p>
+                            </div>
+                        </Link>
+                        <Link
+                            href="yaml-gen/option"
+                            style={{ textDecoration: "underline", color: "white" }}
+                        >
+                            <div className={styles.smallbox}>
+                                <img src="/arrow.png" />
+                                <p className={styles.boxText}>Select</p>
+                            </div>
+                        </Link>
+                        <Link href="yaml-gen/option" style={{ textDecoration: "underline", color: "white" }}>
+                            <div className={styles.smallbox}>
+                                <img src="/arrow.png" />
+                                <p className={styles.boxText}>Init</p>
+                            </div>
+                        </Link>
+                        <Link
+                            href="yaml-gen/option"
+                            style={{ textDecoration: "underline", color: "white" }}
+                        >
+                            <div className={styles.smallbox}>
+                                <img src="/arrow.png" />
+                                <p className={styles.boxText}>Confirm</p>
+                            </div>
+                        </Link>
+                        <Link href="yaml-gen/option" style={{ textDecoration: "underline", color: "white" }}>
+                            <div className={styles.smallbox}>
+                                <img src="/arrow.png" />
+                                <p className={styles.boxText}>Status</p>
+                            </div>
+                        </Link>
+                    </div>
+                    <div className={styles.secondBoxesContainer}>
+                        <Link
+                            href="yaml-gen/option"
+                            style={{ textDecoration: "underline", color: "white" }}
+                        >
+                            <div className={styles.smallbox}>
+                                <img src="/arrow.png" />
+                                <p className={styles.boxText}>Track</p>
+                            </div>
+                        </Link>
+                        <Link
+                            href="yaml-gen/option"
+                            style={{ textDecoration: "underline", color: "white" }}
+                        >
+                            <div className={styles.smallbox}>
+                                <img src="/arrow.png" />
+                                <p className={styles.boxText}>Cancel</p>
+                            </div>
+                        </Link>
+                        <Link href="yaml-gen/option" style={{ textDecoration: "underline", color: "white" }}>
+                            <div className={styles.smallbox}>
+                                <img src="/arrow.png" />
+                                <p className={styles.boxText}>Update</p>
+                            </div>
+                        </Link>
+                        <Link
+                            href="yaml-gen/option"
+                            style={{ textDecoration: "underline", color: "white" }}
+                        >
+                            <div className={styles.smallbox}>
+                                <img src="/arrow.png" />
+                                <p className={styles.boxText}>Rating</p>
+                            </div>
+                        </Link>
+                        <Link href="yaml-gen/option" style={{ textDecoration: "underline", color: "white" }}>
+                            <div className={styles.smallbox}>
+                                <img src="/arrow.png" />
+                                <p className={styles.boxText}>Support</p>
+                            </div>
+                        </Link>
+                    </div>
+
+                </div>
+            </main>
+        </>
+    )
+}
