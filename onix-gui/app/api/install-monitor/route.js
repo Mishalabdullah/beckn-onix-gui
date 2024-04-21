7a4b3be5d23f35bb65212ea0b9f771e4f766908e
Vishakh Abhayan <vishakhabhayan0@gmail.com>
2024-04-21 06:58:56 +0530

    fix:prometheus insall api syntax

--- onix-gui/app/api/install-monitor/route.js
+++ onix-gui/app/api/install-monitor/route.js
@@ -1,32 +1,33 @@
 import { execSync } from "child_process";
 import os from "os";
+import { NextResponse } from "next/server";
 
-export default function handler(req, res) {
-  if (req.method === "POST") {
-    try {
-      const platform = os.platform();
+// export const config = {
+//   runtime: "edge",
+// };
 
-      if (platform === "darwin") {
-        execSync("brew install prometheus", { stdio: "inherit" });
-      } else if (platform === "linux") {
-        execSync("sudo apt-get update && sudo apt-get install prometheus", {
-          stdio: "inherit",
-        });
-      } else {
-        res
-          .status(500)
-          .send(
-            `Installation not supported automatically for ${platform}. Please install Prometheus manually.`
-          );
-        return;
-      }
-
-      res.status(200).send("Prometheus has been installed successfully.");
-    } catch (error) {
-      res.status(500).send(`Failed to install Prometheus: ${error.message}`);
+export default async function POST(req) {
+  try {
+    const platform = os.platform();
+    if (platform === "darwin") {
+      execSync("brew install prometheus", { stdio: "inherit" });
+    } else if (platform === "linux") {
+      execSync("sudo apt-get update && sudo apt-get install prometheus", {
+        stdio: "inherit",
+      });
+    } else {
+      return new NextResponse(
+        `Installation not supported automatically for ${platform}. Please install Prometheus manually.`,
+        { status: 500 }
+      );
     }
-  } else {
-    res.setHeader("Allow", ["POST"]);
-    res.status(405).end(`Method ${req.method} Not Allowed`);
+
+    return new NextResponse("Prometheus has been installed successfully.", {
+      status: 200,
+    });
+  } catch (error) {
+    return new NextResponse(`Failed to install Prometheus: ${error.message}`, {
+      status: 500,
+    });
   }
 }
