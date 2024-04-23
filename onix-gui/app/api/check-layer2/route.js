e6a7707e43c96daf4bb493b010aa44e1198177f2
Mishalabdullah <github.ijvob@simplelogin.co>
2024-04-23 15:17:06 +0530

    route.js file cleaned

--- onix-gui/app/api/check-layer2/route.js
+++ onix-gui/app/api/check-layer2/route.js
@@ -19,53 +19,11 @@ export async function POST(req) {
     });
   };
   try {
-    const result1 = await executeCommand(
-      `docker ps -aqf name=${containerName}`
-    );
-
-    const result2 = await executeCommand(
+    const result = await executeCommand(
       `docker exec ${containerName}  ls /usr/src/app/schemas/ | grep -v "core" | grep ".yaml" | wc -l`
     );
-    // const { stdout, stderr } = await new Promise((resolve, reject) => {
-    //   const commands = [
-    //     `docker ps -aqf name=${containerName}`,
-    //     `docker exec ${containerName}  ls /usr/src/app/schemas/ | grep -v "core" | grep ".yaml" | wc -l`,
-    //   ];
 
-    //   let allStdout = "";
-    //   let allStderr = "";
-
-    //   const runCommand = (index) => {
-    //     if (index >= commands.length) {
-    //       resolve({ stdout: allStdout, stderr: allStderr });
-    //       return;
-    //     }
-
-    //     const command = commands[index];
-    //     exec(command, (error, stdout, stderr) => {
-    //       if (error) {
-    //         reject(error);
-    //       } else {
-    //         allStdout += stdout;
-    //         allStderr += stderr;
-    //         runCommand(index + 1);
-    //       }
-    //     });
-    //   };
-
-    //   runCommand(0);
-    // });
-
-    // const yamlFiles = parseInt(stdout.trim(), 10);
-    // console.log(yamlFiles, "yaml");
-
-    // if (isNaN(yamlFiles)) {
-    //   return new NextResponse(`Invalid output: ${stdout}`, {
-    //     status: 400,
-    //   });
-    // }
-
-    return new NextResponse(JSON.stringify({ result2 }));
+    return new NextResponse(JSON.stringify({ result }));
   } catch (error) {
     console.error(`exec error: ${error}`);
     return new NextResponse(`Error executing shell command: ${error}`, {
