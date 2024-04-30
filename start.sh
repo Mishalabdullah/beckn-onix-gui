0dadad3c3907d49e2c0e249ad1e53f9711a91fbc
Mishalabdullah <github.ijvob@simplelogin.co>
2024-04-30 10:52:29 +0530

    Working Start Script

--- start.sh
+++ start.sh
@@ -1,13 +1,50 @@
 #!/bin/bash
 
-# Change into the onix-gui directory
-cd onix-gui
+# Set script variables
+PROJECT_DIR="onix-gui"
+PORT=3005
+TUNNEL_SERVICE="lt"
+
+# Change to the project directory
+cd "$PROJECT_DIR" || exit
 
 # Build and start the Next.js app
-npx next build && npx next start -p 3005 &
+echo "Building and starting Next.js app..."
+npx next build &&
+echo "Builing Web App = True"
+npx next start -p "$PORT" > /dev/null 2>&1 &
 
-# Install localtunnel globally
-npm install -g localtunnel
+# Wait for the Next.js app to start
+echo "Waiting for Next.js app to start on port $PORT..."
+until nc -z localhost "$PORT"; do
+  sleep 1
+  echo "Loding ..."
+done
 
-# Expose the 3005 port
-lt --port 3005
+# Install the tunnel service if not installed
+if ! command -v "$TUNNEL_SERVICE" &> /dev/null; then
+    echo "Installing $TUNNEL_SERVICE..."
+    npm install -g "$TUNNEL_SERVICE"
+fi
+
+
+
+echo "Exposing local port $PORT using $TUNNEL_SERVICE..."
+lt --port "$PORT" > /tmp/lt.log 2>&1 &
+
+# Wait for the tunnel service to start
+echo "Waiting for tunnel service to start..."
+sleep 5
+
+# Get the tunnel URL from the log file
+TUNNEL_URL=$(grep -o 'https://[^[:blank:]]*' /tmp/lt.log)
+#Get the tunnel password
+echo "Getting Tunnel Password"
+TUNNEL_PASSWORD=$(curl https://loca.lt/mytunnelpassword)&&
+
+# Print the tunnel URL and password
+echo "---------------------------------------"
+echo "Next.js app is running locally on port $PORT"
+echo "Tunnel Service URL: $TUNNEL_URL"
+echo "Tunnel Password: $TUNNEL_PASSWORD"
+echo "---------------------------------------"
