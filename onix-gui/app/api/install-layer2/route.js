41aaff533cbc8372ccb3641956a7ce8f5ae01d18
Mishalabdullah <github.ijvob@simplelogin.co>
2024-05-05 23:09:52 +0530

    updated yaml install  script

--- onix-gui/app/api/install-layer2/route.js
+++ onix-gui/app/api/install-layer2/route.js
@@ -29,13 +29,18 @@ export async function POST(req) {
   try {
     await executeShellCommand(
       `docker exec ${
-        containerName + "-network"
-      } wget -P /usr/src/app/schemas/  ${fileURL}`
+        containerName + "-client"
+      } wget -P /usr/src/app/schemas/ ${fileURL}`
     );
+  } catch (error) {
+    console.error(`exec error: ${error}`);
+  }
+
+  try {
     await executeShellCommand(
       `docker exec ${
-        containerName + "-client"
-      } wget -P /usr/src/app/schemas/  ${fileURL}`
+        containerName + "-network"
+      } wget -P /usr/src/app/schemas/ ${fileURL}`
     );
     return NextResponse.json({ status: 200 });
   } catch (error) {
